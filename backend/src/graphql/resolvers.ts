bb05ef8f60d4b00733dee6dcca0c0e106d09e7ef
Sergio Pinzon <sergiopin596@gmail.com>
2025-08-22 20:30:29 -0500

    refactor: resolvers is now a class

--- backend/src/graphql/resolvers.ts
+++ backend/src/graphql/resolvers.ts
@@ -1,34 +1,38 @@
 import { Op } from 'sequelize'
 import Character from '../models/Character'
 
+class Resolvers {
+    static async characters(_: any, args: any) {
+        const filters: any = {};
+
+        if (args.name) {
+            filters.name = { [Op.iLike]: `%${args.name}%` }
+        }
+        if (args.status) {
+            filters.status = { [Op.iLike]: args.status }
+        }
+        if (args.species) {
+            filters.species = { [Op.iLike]: args.species }
+        }
+        if (args.gender) {
+            filters.gender = { [Op.iLike]: args.gender }
+        }
+        if (args.origin) {
+            filters.origin = { [Op.iLike]: args.origin }
+        }
+
+        return await Character.findAll({ where: filters });
+    }
+
+    static async character(_: any, { id }: { id: number }) {
+        return await Character.findByPk(id);
+    }
+}
+
 const resolvers = {
-
     Query: {
-        characters: async (_:any, args:any) => {
-            const filters: any = {};
-
-            if (args.name){
-                filters.name = { [Op.iLike]: `%${args.name}%`}
-            }
-            if (args.status){
-                filters.status = { [Op.iLike]: args.status}
-            }
-            if (args.species){
-                filters.species = { [Op.iLike]: args.species}
-            }
-            if (args.gender){
-                filters.gender = { [Op.iLike]: args.gender}
-            }
-            if (args.origin){
-                filters.origin = { [Op.iLike]: args.origin}
-            }
-
-            return await Character.findAll({where: filters});
-        },
-
-        character: async(_:any, {id}:{id:number}) => {
-            return await Character.findByPk(id);
-        },
+        characters: Resolvers.characters,
+        character: Resolvers.character,
     },
 };
 
